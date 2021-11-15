c037f4477fbc49c4d8401dc40aeb063854ebfdc8
Dongsheng Wen <wen94@purdue.edu>
2021-11-15 20:38:42 +0800

    removed sspredict folder

--- sspredict/make_prediction/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .read_input import master_read_input
-from .read_input import *
-from .input_format import input_format
-from .make_prediction import make_prediction
-from .make_composition import build_mesh_ternary
-from .models import *
